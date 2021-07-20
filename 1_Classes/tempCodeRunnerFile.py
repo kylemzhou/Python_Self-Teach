b3b14f97dc6eb9bca62d64cc576d03c7d3782886
Kyle Zhou <75555454+kylemzhou@users.noreply.github.com>
2021-07-20 15:02:35 -0700

    Delete tempCodeRunnerFile.py

--- 1_Classes/tempCodeRunnerFile.py
+++
@@ -1,8 +0,0 @@
-if question == 'no':
-            while loop:
-                category = input("What is new category?: \n")
-                category = category[0].upper() + category[1:].lower()
-                price = float(input("How much money do you spent in this category?: \n"))
-                Receipts(category,price).add_obj_category()
-                break
-            loop = False
